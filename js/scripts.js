abc3bae0ea263a3b913303b9418aa9782ad6685f
Chris Finney <the.finney@gmail.com>
2017-02-24 11:24:15 -0800

    create function to replace array elements divisible by 3 with 'ping'

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
 //Back-End Logic
 
-
-// var pingPong = function(input) {
-//
-//
-//   numberToArray(input);
-//   console.log(input);
-//
-// };
-
 var numberToArray = function(input) {
   var inputArray = [];
+
   for (i = 1; i <= input; i++) {
     inputArray.push(i);
   }
-};
+
+  var ind = 0;
+  inputArray.forEach(function(element){
+
+    if (element % 3 === 0) {
+        inputArray.splice(ind, 1, "ping");
+        ind++;
+    } else {
+      ind ++;
+    };
+  });
+  console.log(inputArray);
+    // if ((element%3 === 0) && (element%5 === 0)) {
+    //     inputArray.splice(ind, 1, "pingpong");
+    // } else if (element%3 === 0) {
+    //     inputArray.splice(ind, 1, "ping");
+    // } else if (element%5 === 0) {
+    //     inputArray.splice(ind, 1, "pong");
+    // } else {
+    //   ;
+    // }
+    // ind++;
+  };
 
 // Front-End (UI)
 $(document).ready(function(){
@@ -25,5 +39,6 @@ $(document).ready(function(){
     var result = numberToArray(userInput);
 
     $("#result").text(result);
+
   });
 });
